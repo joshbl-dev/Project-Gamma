211d44834a1cf2a15e8ddb1061671d6d79c02cb7
D_Diamonds <laxisgood4u@gmail.com>
2019-09-10 11:52:55 -0400

    Folders
    Folder structure and some required setup

---
+++ main.lua
@@ -0,0 +1,22 @@
+require 'src/Dependencies'
+
+function love.load()
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+
+    love.keyboard.keysPressed[key] = true
+end
+
+function love.keyboard.wasPressed(key)
+    return love.keyboard.keysPressed[key]
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+end
