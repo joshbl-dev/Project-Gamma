ad1aa681a7016040b377b7edbcd55208e4fe2473
D_Diamonds <laxisgood4u@gmail.com>
2019-09-24 10:30:08 -0400

    playstate exit button

--- src/States/PlayState.lua
+++ src/States/PlayState.lua
@@ -5,14 +5,20 @@ function PlayState:init()
 		rows = 5,
 		cols = 5
 	}
+	self.tempMenu = Button(0, 50, 120, 50, "Menu")
 end
 
 function PlayState:update(dt)
 	self.grid:update(dt)
+	self.tempMenu:update(dt)
+	if self.tempMenu.clickable.pressed then
+		stateMachine:change("start")
+	end
 end
 
 function PlayState:render()
 	self.grid:render()
+	self.tempMenu:render()
 end
 
 
