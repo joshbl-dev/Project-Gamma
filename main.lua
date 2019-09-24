037a24d3705767350651282a02f43ab16b7907c8
Tyler <tjharper2020@gmail.com>
2019-09-24 10:11:07 -0400

    proposed pressed changes

--- main.lua
+++ main.lua
@@ -69,3 +69,28 @@ function displayFPS()
     love.graphics.setColor(colors['black'])
     love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()), 0, 0)
 end
+
+--[[
+function Button:update(dt)
+	local x, y = love.mouse.getPosition()
+	clickedDown
+    if x > self.x and x < self.x + self.width and y > self.y and y < self.y + self.height then
+        if love.mouse.isDown(1) then
+            clickedDown = true
+        elseif clickedDown = true then
+        	self.pressed = true
+        	clickedDown = false
+        else
+        	self.pressed = false
+        end
+        self.hovering = true
+    else
+        self.hovering = false
+    end
+end
+]]
+
+
+
+
+
