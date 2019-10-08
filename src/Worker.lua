e6d28644d4bb8318aafaaf1096ed8b2464c9b215
Tyler <tjharper2020@gmail.com>
2019-10-08 10:07:59 -0400

    money increase and decrease per worker

--- src/Worker.lua
+++ src/Worker.lua
@@ -11,19 +11,28 @@ function Worker:init(defs)
 	self.timeEmployed = defs.worker[3] -- timeEmployed
 
 	self.wclock = Clock(self.x, self.y + 10, true, self.timeEmployed)
+	self.lastDay = 0
+	self.lastMonth = 0
+	self.rev = DEFAULT_REV
 end
 
 function Worker:update(dt)
-	-- if self.purchased == true then
+	self.lastDay = self.wclock.day
+	self.lastMonth = self.wclock.month
 	self.wclock:update(dt)
+	if self.lastDay ~= self.wclock.day then
+		money = money + self.rev
+	end
+	if self.lastMonth ~= self.wclock.month then
+		money = money - self.salary
+	end
 	self.timeEmployed = self.wclock.time
-	-- end
 end
 
 function Worker:render()
 	if self.purchased then
 		love.graphics.setColor(colors["green"])
 		love.graphics.circle("fill", self.x + self.width * .25 + self.size, self.y + self.height * .75 / 2, self.size)
-		--self.wclock:render()
+		
 	end
 end
