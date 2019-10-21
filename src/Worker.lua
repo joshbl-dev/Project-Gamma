6c26a4d919646760feafbe796669e6bc78664585
Tyler <tjharper2020@gmail.com>
2019-10-20 21:15:47 -0400

    revenue for worker scaled

--- src/Worker.lua
+++ src/Worker.lua
@@ -12,7 +12,7 @@ function Worker:init(defs)
 	self.wclock = Clock(self.x, self.y + 10, true, self.timeEmployed)
 	self.lastDay = self.wclock.day
 	self.lastMonth = self.wclock.month
-	self.rev = DEFAULT_REV
+	self.rev = DEFAULT_REV * (math.pow(1.5, defs.floorNum - 1))
 end
 
 function Worker:update(dt)
