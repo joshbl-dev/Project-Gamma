37578d0a446be722015f7d84a617ae814fbb4eba
Tyler <tjharper2020@gmail.com>
2019-12-09 10:28:11 -0500

    added back button

--- src/States/AchievementState.lua
+++ src/States/AchievementState.lua
@@ -1,13 +1,28 @@
 AchievementState = Class{__includes = BaseState}
 
 function AchievementState:init()
+	local buttonWidth = 120
+	local buttonHeight = 50
 
+	self.buttons = {
+		["back"] = Button ({ x = VIRTUAL_WIDTH - (buttonWidth + 10),
+			y = 10,
+			width = buttonWidth,
+			height = buttonHeight,
+			text = "Back", onClick = function()
+				stateMachine:change("start")
+			end}, "rect")
+	}
 end
 
 function AchievementState:update(dt)
-
+	for i, button in pairs(self.buttons) do
+		button:update(dt)
+	end
 end
 
 function AchievementState:render()
-
+for i, button in pairs(self.buttons) do
+		button:render()
+	end
 end
