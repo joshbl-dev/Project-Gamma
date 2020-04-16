33e7f2110609214328e3f0ce804c18b276f65d62
D_Diamonds <D-Diamonds@users.noreply.github.com>
2020-04-16 09:04:09 -0400

    worker_defs

---
+++ src/worker_defs.lua
@@ -0,0 +1,42 @@
+WORKER_DEFS = {
+    ['player'] = {
+        animations = {
+            ['walk-down'] = {
+                frames = {2, 1, 3},
+                interval = 0.15,
+                texture = 'player'
+            },
+            ['walk-up'] = {
+                frames = {5, 4, 6},
+                interval = 0.15,
+                texture = 'player'
+            },
+            ['walk-left'] = {
+                frames = {7, 8, 9},
+                interval = 0.15,
+                texture = 'player'
+            },
+            ['walk-right'] = {
+                frames = {10, 11, 12},
+                interval = 0.15,
+                texture = 'player'
+            },
+            ['idle-left'] = {
+                frames = {7},
+                texture = 'player'
+            },
+            ['idle-right'] = {
+                frames = {10},
+                texture = 'player'
+            },
+            ['idle-down'] = {
+                frames = {1},
+                texture = 'player'
+            },
+            ['idle-up'] = {
+                frames = {4},
+                texture = 'player'
+            },
+        }
+    }
+}
